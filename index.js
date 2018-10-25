0e72f22e32c69f4f21ff10b68d232f23f797ac8e
Peter Downing <petedowning14@gmail.com>
2018-10-25 09:19:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = {}
+  for (var i = 0; i < array.length; i++) {
+    var string = array[i] + " plays " + instruments[i]
+    array.push(string)
+  }
+  return array
+}
+
+function johnLennonFacts(facts) {
+  var j = facts.length
+  while (j > 0) {
+    facts[j-1] = facts[j-1] + "!!!"
+    j--
+  }
+}
